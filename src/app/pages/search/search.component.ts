0f28f36c46e705b65db2acd1175efbf0baf1442a
Helene <helene.blanche@hotmail.fr>
2023-09-13 00:56:35 +0200

    Add search component

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ArticlesService } from '../../services/articles.service';
+import { Article } from '../../models/Article';
 
 @Component({
   selector: 'app-search',
@@ -8,6 +9,11 @@ import { ArticlesService } from '../../services/articles.service';
 })
 export class SearchComponent implements OnInit {
   articles: any[] = [];
+  articleSearch = '';
+  articleData!: Article[];
+  author!: string;
+  tag!: string;
+  title!: string;
   constructor(private articlesService: ArticlesService) {}
   getArticles(): void {
     this.articlesService.getArticles().subscribe((articles: any) => {
@@ -18,5 +24,37 @@ export class SearchComponent implements OnInit {
     this.articlesService.getArticles().subscribe((articles: any) => {
       this.articles = articles;
     });
+  }
+
+  onAuthorSearchChange() {
+    this.articlesService.getArticlesByAuthor(this.author).subscribe(
+      (articles: Article[]) => {
+        this.articles = articles;
+      },
+      (error) => {
+        console.error("Une erreur s'est produite :", error);
+      }
+    );
+  }
+  onTagSearchChange() {
+    this.articlesService.getArticlesByTag(this.tag).subscribe(
+      (articles: Article[]) => {
+        this.articles = articles;
+        this.articles.includes(this.tag);
+      },
+      (error) => {
+        console.error("Une erreur s'est produite :", error);
+      }
+    );
+  }
+  onTitleSearchChange() {
+    this.articlesService.getArticlesByTitle(this.title).subscribe(
+      (articles: Article[]) => {
+        this.articles = articles;
+      },
+      (error) => {
+        console.error("Une erreur s'est produite :", error);
+      }
+    );
   }
 }
--- src/app/services/articles.service.ts
+++ src/app/services/articles.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { Article } from '../models/Article';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -14,5 +16,20 @@ export class ArticlesService {
   }
   getArticleById(id: string) {
     return this.httpClient.get(this.articlesDataUrl + '/' + id);
+  }
+  getArticlesByAuthor(author: string): Observable<Article[]> {
+    return this.httpClient.get<Article[]>(
+      `${this.articlesDataUrl}/byAuthor/${author}`
+    );
+  }
+  getArticlesByTag(tag: string): Observable<Article[]> {
+    return this.httpClient.get<Article[]>(
+      `${this.articlesDataUrl}/byTag/${tag}`
+    );
+  }
+  getArticlesByTitle(title: string): Observable<Article[]> {
+    return this.httpClient.get<Article[]>(
+      `${this.articlesDataUrl}/byTitle/${title}`
+    );
   }
 }
