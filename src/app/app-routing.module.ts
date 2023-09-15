409ff739ea5ac3e4289381e70d7fd9cc87adbb1e
Heleneb1 <helene.blanche@hotmail.fr>
2023-09-15 13:04:20 +0200

    commit for add quill.js

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,46 @@
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+// import { LoginAuthGuardService } from './services/login-auth-guard.service';
+// import { ConnectionComponent } from './pages/connection/connection.component';
+// import { HomeComponent } from './pages/home/home.component';
+// import { SplashPageComponent } from './pages/splash-page/splash-page.component';
+// import { SearchComponent } from './pages/search/search.component';
+
+// const routes: Routes = [
+//   {
+//     path: '',
+//     component: SplashPageComponent,
+//   },
+//   {
+//     path: 'home',
+//     component: HomeComponent,
+//   },
+//   {
+//     path: 'article',
+//     component: SearchComponent,
+//   },
+//   {
+//     path: 'authentication',
+//     component: ConnectionComponent,
+//     canActivate: [LoginAuthGuardService],
+//   },
+// ];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule],
+// })
+// export class AppRoutingModule {}
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginAuthGuardService } from './services/login-auth-guard.service';
 import { ConnectionComponent } from './pages/connection/connection.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SplashPageComponent } from './pages/splash-page/splash-page.component';
 import { SearchComponent } from './pages/search/search.component';
+// import { AdminComponent } from './pages/admin/admin.component'; // Supposez que vous avez une page d'administration
+
+// Importez votre AdminAuthGuardService
+// import { AdminAuthGuardService } from './services/admin-auth-guard.service';
 
 const routes: Routes = [
   {
@@ -22,8 +58,12 @@ const routes: Routes = [
   {
     path: 'authentication',
     component: ConnectionComponent,
-    canActivate: [LoginAuthGuardService],
   },
+  // {
+  //   path: 'admin',
+  //   component: AdminComponent, // Page d'administration
+  //   canActivate: [AdminAuthGuardService], // Garde pour l'accès administratif
+  // },
 ];
 
 @NgModule({
