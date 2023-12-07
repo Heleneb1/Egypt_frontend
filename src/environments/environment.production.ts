5722ee9526e12b96f1c881083d44a60ca39eb4a6
Heleneb1 <helene.blanche@hotmail.fr>
2023-12-07 23:01:27 +0100

    clean scss

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,4 @@
 export const environment = {
   //TODO modifier l'apiUrl pour qu'il pointe vers l'api de votre projet
-  //  apiUrl: 'https:lesmysteresdelegypteantique.fr',
+  apiUrl: 'https://egypt-api.lesmysteresdelegypteantique.fr',
 };
-//m
