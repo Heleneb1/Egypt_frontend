419e284ee5596b651bfcb5dd2671416241fd917a
Heleneb1 <helene.blanche@hotmail.fr>
2023-12-07 16:25:42 +0100

    Fix scss too large for deployment

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   //TODO modifier l'apiUrl pour qu'il pointe vers l'api de votre projet
-  // apiUrl: 'https://brainwave-api.bastienvps.ddns.net',
+  //  apiUrl: 'https:lesmysteresdelegypteantique.fr',
 };
-//mysteresdelegypte.ddns.net
+//m
