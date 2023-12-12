c1823650d447b45063059109a5cc3b6a669583c6
Heleneb1 <helene.blanche@hotmail.fr>
2023-12-12 16:57:45 +0100

    Update without quill.js which causes vulnerabilities

--- src/app/components/create-quiz/create-quiz.component.ts
+++ src/app/components/create-quiz/create-quiz.component.ts
@@ -1,96 +1,119 @@
-import { Component, AfterViewInit, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { environment } from 'src/environments/environment.development';
-import Quill from 'quill';
-import 'quill-emoji/dist/quill-emoji.js';
 import { ToastrService } from 'ngx-toastr';
+import { QuizService } from 'src/app/services/quiz.service';
 
 @Component({
   selector: 'app-create-quiz',
   templateUrl: './create-quiz.component.html',
   styleUrls: ['./create-quiz.component.scss']
 })
-export class CreateQuizComponent implements AfterViewInit, OnInit {
+export class CreateQuizComponent implements OnInit {
 
   showModal = false;
-  quill!: Quill;
   textValue: string = '';
-  description: string = '';
   picture: string = '';
   tag: string = '';
-  type: string = '';
   difficulty: string = '';
-  createdQuizId: string = '';
   userConnected: any;
   userId: string = '';
   isArchived: boolean = true;
-
+  descriptionText: string = '';
+  question_title: string = '';
+  option_1: string = '';
+  option_2: string = '';
+  option_3: string = '';
+  right_answer: string = '';
+  right_answer_2: string = '';
+  category: string = '';
+  createdQuestions: any[] = [];
+  createdQuiz: any;
+  formValid: boolean = false;
 
   constructor (
     private http: HttpClient,
     private router: Router,
     private authService: AuthService,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private quizService: QuizService,
   ) {
     this.userConnected = this.authService.getUserConnected();
     this.authService.getUserConnected().subscribe((user: any) => {
       this.userConnected = user;
       this.userId = user?.userId;
-
     });
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+  checkFormValidity(): boolean {
+    // Vérifiez ici si tous les champs nécessaires sont remplis
+    return !!this.textValue && !!this.tag && !!this.difficulty && !!this.picture && this.createdQuestions.length > 4;
   }
-
-  ngAfterViewInit() {
-    const toolbarOptions = [
-      ['bold', 'italic', 'underline', 'strike', 'blockquote', 'code-block', 'script', 'header',
-        'indent', 'list', 'direction', 'align', 'clean'],
-      ['emoji', 'link', 'image', 'video', 'formula', 'code', 'table'],
-      [{ color: [] }, { background: [] }],
-      ['clean'],
-      [{ size: ['small', false, 'large', 'huge'] }],
-
-    ];
-
-
-    this.quill = new Quill('#editor', {
-      modules: {
-        toolbar: toolbarOptions,
-      },
-      theme: 'snow',
-    });
-  }
-
   createQuiz(): void {
-    const descriptionText = this.quill.root.innerHTML;
-    const userId = this.userConnected.id;
-    const url = environment.apiUrl + `/quizzes/create/${userId}`;
+    if (this.checkFormValidity()) {
+      const userId = this.userConnected.id;
+      const url = environment.apiUrl + `/quizzes/create/${userId}`;
 
-    const data = {
-      content: descriptionText,
-      title: this.textValue,
-      picture: this.picture,
-      tag: this.tag,
-      difficulty: this.difficulty,
-      archive: this.isArchived,
-    };
+      const data = {
+        content: this.descriptionText,
+        title: this.textValue,
+        picture: this.picture,
+        tag: this.tag,
+        difficulty: this.difficulty,
+        archive: this.isArchived,
+      };
 
-    this.http.post(url, data).subscribe(
+      this.http.post(url, data).subscribe(
+        (response: any) => {
+          console.log('Quiz created', response);
+          this.createdQuiz = response;  // Sauvegardez le quiz créé
+          this.addQuestionsToQuiz(this.category);  // Ajoutez les questions au quiz
+        },
+        (error) => {
+          console.error('Failed to create quiz', error);
+        }
+      );
 
-      (response: any) => {
-      },
-      (error) => {
-        console.error('Failed to create quiz', error);
 
-      }
-    );
-    this.resetForm();
-    this.router.navigate(['/profile']);
-    this.toastr.success('Le quiz est créé avec succès !');
+      this.resetForm();
+      this.toastr.success('Le quiz est créé avec succès !');
+    } else {
+      // Affichez un message d'erreur ou prenez toute autre action nécessaire
+      this.toastr.error('Veuillez remplir tous les champs obligatoires.');
+    }
+  }
+  createQuestion() {
+    this.question_title = '';
+    this.option_1 = '';
+    this.option_2 = '';
+    this.option_3 = '';
+    this.right_answer = '';
+    this.right_answer_2 = '';
+    // this.category = '';
+  }
+
+  onCreateQuestion() {
+    if (!this.category) {
+      this.toastr.error('La catégorie ne peut pas être vide.', 'Erreur');
+    } else {
+      const newQuestion = {
+        question_title: this.question_title,
+        option_1: this.option_1,
+        option_2: this.option_2,
+        option_3: this.option_3,
+        right_answer: this.right_answer,
+        right_answer_2: this.right_answer_2,
+        category: this.category,
+      };
+
+      this.quizService.createQuestion(newQuestion).subscribe((response: any) => {
+        this.createQuestion();
+      });
+      this.createdQuestions.push(newQuestion);
+    }
   }
 
   resetForm(): void {
@@ -99,10 +122,19 @@ export class CreateQuizComponent implements AfterViewInit, OnInit {
     this.tag = '';
     this.difficulty = '';
     this.isArchived = true;
-    this.quill.root.innerHTML = '';
+    this.descriptionText = '';
   }
+  addQuestionsToQuiz(category: string) {
+    if (this.createdQuiz && this.createdQuestions.length > 0) {
+      console.log("quizId", this.createdQuiz.id);
+      console.log("questions", this.createdQuestions);
+      console.log("category", category);
+
+      this.quizService.addQuestionByCategoryToQuiz(this.createdQuiz.id, category).subscribe((response: any) => {
+        this.toastr.success('Questions ajoutées au quiz', 'Ajout');
+      });
+    }
+  }
+
 }
-
-
-
 
