9197abd9af351f6ccbc0ca056c8d3e68417b139f
Heleneb1 <helene.blanche@hotmail.fr>
2023-12-13 12:02:20 +0100

    add changes

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  //TODO modifier l'apiUrl pour qu'il pointe vers l'api de votre projet
+  // modifier l'apiUrl pour qu'il pointe vers l'api de votre projet
   apiUrl: 'https://egypt-api.lesmysteresdelegypteantique.fr',
 };
