4de88dc0c6dc6e190059b45241a15af905e09fb9
Heleneb1 <helene.blanche@hotmail.fr>
2024-01-21 15:09:49 +0100

    Test

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  apiUrl: 'https://egypt-api.lesmysteresdelegypteantique.fr',
+  apiUrl: 'http://localhost:8080',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  apiUrl: 'http://localhost:8080',
+
+  apiUrl: 'https://egypt-api.lesmysteresdelegypteantique.fr',
 };
