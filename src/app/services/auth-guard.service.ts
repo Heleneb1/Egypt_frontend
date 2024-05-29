1295209b4a839d3cfedac34a0c8456386dde1dd4
HeleneB <helene.blanche@hotmail.fr>
2024-05-29 17:15:19 +0200

    fix

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,24 +1,32 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
 import { AuthService } from './auth.service';
 import { Observable, map, of, tap } from 'rxjs';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor (private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(_route: ActivatedRouteSnapshot, _state: RouterStateSnapshot): Observable<boolean> {
-    return this.authService.isLoggedIn() ? this.authService.getUserRole().pipe(
-      map(userRole => {
-
-        if (userRole === 'USER') {
-
-          return true;
-        } else {
-          this.router.navigate(['/authentication']);
-          return false;
-        }
-      })
-    ) : of(false).pipe(tap(() => this.router.navigate(['/authentication'])));
+  canActivate(
+    _route: ActivatedRouteSnapshot,
+    _state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return this.authService.isLoggedIn()
+      ? this.authService.getUserRole().pipe(
+          map((userRole) => {
+            if (userRole === 'USER') {
+              return true;
+            } else {
+              this.router.navigate(['/authentication']);
+              return false;
+            }
+          })
+        )
+      : of(false).pipe(tap(() => this.router.navigate(['/authentication'])));
   }
 }
