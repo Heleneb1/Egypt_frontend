1dc573aa50af22acab8ea6a1dcb0c9d20ec24672
HeleneB <helene.blanche@hotmail.fr>
2024-06-07 23:16:23 +0200

    fix bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,28 @@
 import { Component } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
-import { NgcCookieConsentService, NgcInitializationErrorEvent, NgcInitializingEvent, NgcNoCookieLawEvent, NgcStatusChangeEvent } from 'ngx-cookieconsent';
+import {
+  NgcCookieConsentService,
+  NgcInitializationErrorEvent,
+  NgcInitializingEvent,
+  NgcNoCookieLawEvent,
+  NgcStatusChangeEvent,
+} from 'ngx-cookieconsent';
 import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  title = "Les Mystères de l'Égypte Antique ";
   showHeader: boolean = true;
 
-  constructor (private router: Router, private ccService: NgcCookieConsentService) {
-    this.router.events.subscribe(event => {
+  constructor(
+    private router: Router,
+    private ccService: NgcCookieConsentService
+  ) {
+    this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
         this.showHeader = router.url !== '/';
       }
@@ -28,57 +38,60 @@ export class AppComponent {
   private revokeChoiceSubscription!: Subscription;
   private noCookieLawSubscription!: Subscription;
 
-
   ngOnInit() {
     // Delayed initialization of the cookie consent banner
     setTimeout(() => {
       const config = this.ccService.getConfig();
       if (config && config.content) {
-        config.content.message = "Un petit cookie pour la route?\n Avant de s'immerger dans l'Égypte des Pharaons ?",
+        (config.content.message =
+          "Un petit cookie pour la route?\n Avant de s'immerger dans l'Égypte des Pharaons ?"),
           this.ccService.init(config); // Initialize the cookie consent banner
       }
     }, 5000);
     // subscribe to cookieconsent observables to react to main events
-    this.popupOpenSubscription = this.ccService.popupOpen$.subscribe(
-      () => {
-        // you can use this.ccService.getConfig() to do stuff...
-      });
+    this.popupOpenSubscription = this.ccService.popupOpen$.subscribe(() => {
+      // you can use this.ccService.getConfig() to do stuff...
+    });
 
-    this.popupCloseSubscription = this.ccService.popupClose$.subscribe(
-      () => {
-        // you can use this.ccService.getConfig() to do stuff...
-      });
+    this.popupCloseSubscription = this.ccService.popupClose$.subscribe(() => {
+      // you can use this.ccService.getConfig() to do stuff...
+    });
 
     this.initializingSubscription = this.ccService.initializing$.subscribe(
       (event: NgcInitializingEvent) => {
         // the cookieconsent is initilializing... Not yet safe to call methods like `NgcCookieConsentService.hasAnswered()`
-      });
+      }
+    );
 
-    this.initializedSubscription = this.ccService.initialized$.subscribe(
-      () => {
-        // the cookieconsent has been successfully initialized.
-        // It's now safe to use methods on NgcCookieConsentService that require it, like `hasAnswered()` for eg...
-      });
+    this.initializedSubscription = this.ccService.initialized$.subscribe(() => {
+      // the cookieconsent has been successfully initialized.
+      // It's now safe to use methods on NgcCookieConsentService that require it, like `hasAnswered()` for eg...
+    });
 
-    this.initializationErrorSubscription = this.ccService.initializationError$.subscribe(
-      (event: NgcInitializationErrorEvent) => {
-        // the cookieconsent has failed to initialize... 
-      });
+    this.initializationErrorSubscription =
+      this.ccService.initializationError$.subscribe(
+        (event: NgcInitializationErrorEvent) => {
+          // the cookieconsent has failed to initialize...
+        }
+      );
 
     this.statusChangeSubscription = this.ccService.statusChange$.subscribe(
       (event: NgcStatusChangeEvent) => {
         // you can use this.ccService.getConfig() to do stuff...
-      });
+      }
+    );
 
     this.revokeChoiceSubscription = this.ccService.revokeChoice$.subscribe(
       () => {
         // you can use this.ccService.getConfig() to do stuff...
-      });
+      }
+    );
 
     this.noCookieLawSubscription = this.ccService.noCookieLaw$.subscribe(
       (event: NgcNoCookieLawEvent) => {
         // you can use this.ccService.getConfig() to do stuff...
-      });
+      }
+    );
     //https://stackoverflow.com/questions/44309641/angular-2-scroll-to-top-on-every-route-click
     this.router.events.subscribe((evt) => {
       if (!(evt instanceof NavigationEnd)) {
@@ -86,7 +99,6 @@ export class AppComponent {
       }
       window.scrollTo(0, 0);
     });
-
   }
 
   ngOnDestroy() {
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,13 +6,17 @@ import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
   isMenuOpen = false;
   isConnected = false;
 
-  constructor (private router: Router, private authService: AuthService, private toastr: ToastrService) {
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private toastr: ToastrService
+  ) {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
         this.closeMenu();
@@ -28,7 +32,10 @@ export class HeaderComponent implements OnInit {
     this.authService.logout();
     this.isConnected = false;
     this.router.navigate(['/home']);
-    this.toastr.success('Vous venez de vous déconnecter avec succes,\nà bientôt...', 'Déconnexion');
+    this.toastr.success(
+      'Vous venez de vous déconnecter avec succes,\nà bientôt...',
+      'Déconnexion'
+    );
   }
 
   redirectToProfile() {
